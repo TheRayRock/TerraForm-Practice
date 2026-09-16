4590e5fecc7c4b97676b11ccd4eaa820234ef3ee
ThrRayRock <mdshahnawazz0806@gmail.com>
2026-09-16 16:43:07 +0530

    Add EC2 instance count and update output values for multiple instances

--- ec2.tf
+++ ec2.tf
@@ -63,6 +63,7 @@ resource "aws_security_group" "my_security_group" {
 
 resource "aws_instance" "my_instance" {
 
+  count = 2
   key_name        = aws_key_pair.my_key.key_name
   security_groups = [aws_security_group.my_security_group.name]
   instance_type   = var.ec2_intance_type
